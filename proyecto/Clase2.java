b467bee205553f14b1d706ffc7093e46dd63b6bc
JoseOrtizHdz <josejoaquin_1312@alumnos.uaslp.edu.mx>
2015-04-24 11:25:06 -0500

    se sube la clase2

---
+++ proyecto/Clase2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Escribe una descrición de la clase Clase2 aquí.
+ * 
+ * @autor (tu nombre) 
+ * @versión (Un número de versión o una fecha)
+ */
+public class Clase2 extends Actor
+{
+    /**
+     * Act - hace lo que Clase2 quiere hacer. Este método se llama "cuando quiera" o whenever
+     * los botones 'Actuar or 'Ejecutar' son presionados en el entorno.
+     */
+    public void act() 
+    {
+        // Agrega tus códigos de acción aquí.
+    }    
+}
