7a9425c78b919a5119ffe12bc68bc24ae9eb7e0f
OxcarME <yooxcar@gmail.com>
2015-05-17 14:12:44 -0500

    Agregada captura para página Agregada Documentación de la clase Boton

--- proyecto/Boton.java
+++ proyecto/Boton.java
@@ -1,11 +1,17 @@
 import greenfoot.*; 
- /**
- Clase que permite agregar 
- @autor (tu nombre) 
- @versión (Un número de versión o una fecha)
+/**
+ * Esta clase representa un elemento de la interfaz del juego,
+ * es agregado en cada parte de menú.
+ * 
+ * @author José Joaquín Ortiz Hernández
+ * @author Oscar Torres Silva
+ * @version 26/abril/2015
  */
 public class Boton extends Actor
 {
+    /**
+     * Constructor de la clase, inicializa la imágen que utiliza el objeto de esta clase.
+     */
     public Boton(GreenfootImage ima)
     {
         super();
